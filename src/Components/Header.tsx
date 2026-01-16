4f2b5aa6907321b31355756a2e282c0a0f658fa5
John Flynn <jrflynn3@gmail.com>
2026-01-15 21:34:05 -0500

    add shadow to header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,18 +1,16 @@
 import { NavLink } from "react-router";
 import { NavBar } from "./NavBar";
+import logo from "../Assets/Other/JF-logo-orange-gray.png";
 
 export const Header = () => {
   return (
     <div
       id="page-header"
-      className="flex bg-secondary justify-between p-5 sticky transition-[top] duration-300"
+      className="flex bg-secondary justify-between p-5 sticky transition-[top] duration-300 shadow-lg/15"
     >
       <nav>
         <NavLink to="/">
-          <img
-            className="flex h-10 w-auto pl-2.5"
-            src="./src/Assets/JF-logo-orange-gray.png"
-          ></img>
+          <img className="flex h-10 w-auto pl-2.5" src={logo}></img>
         </NavLink>
       </nav>
       <NavBar />
