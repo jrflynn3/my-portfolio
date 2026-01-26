dec390022ff71e2c447709367f74eaf91b4cb5f3
John Flynn <jrflynn3@gmail.com>
2026-01-26 18:42:40 -0500

    update footer with new links

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,18 +1,26 @@
-import { NavLink } from "react-router-dom";
-
 export const Footer = () => {
-  return (
-    <div className="flex bg-tertiary p-5 justify-between">
-      <div className="text-[1.1em] text-primary tracking-[2px]">
-        John Flynn ©
-      </div>
+  const Link = ({ name, link }: { name: string; link: string }) => (
+    <a
+      className="text-[1.1em] text-primary tracking-[2px] hover:underline hover:font-bold"
+      href={link}
+    >
+      {name}
+    </a>
+  );
 
-      <NavLink
-        className="text-[1.1em] text-primary tracking-[2px] hover:underline hover:font-bold"
-        to="mailto:hello@john-flynn.co"
-      >
-        Contact Me
-      </NavLink>
+  return (
+    <div className="flex w-full bg-tertiary p-5 justify-between text-primary">
+      <div className="text-[1.1em] tracking-[2px]">© 2026 John Flynn</div>
+      <div className="flex gap-x-4">
+        <Link name="Let's connect" link="mailto:hello@john-flynn.co" />
+        ·
+        <Link
+          name="LinkedIn"
+          link="https://www.linkedin.com/in/john-flynn-890412154/"
+        />
+        ·
+        <Link name="GitHub" link="https://github.com/jrflynn3" />
+      </div>
     </div>
   );
 };
