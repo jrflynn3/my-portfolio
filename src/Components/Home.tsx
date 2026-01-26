dd0afe9027148ddd8f11940a1c9b491678f89fdf
John Flynn <jrflynn3@gmail.com>
2026-01-26 18:45:19 -0500

    add brief intro to home page with button to about me link

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,20 +1,29 @@
 import { Footer } from "./Footer";
+import GhostButton from "./GhostButton";
 import { Header } from "./Header";
 
 export const Home = () => {
   return (
     <div
-      className={`flex flex-col bg-cover bg-[position:70%_bottom]
-         md:bg-contain md:bg-bottom bg-no-repeat min-h-[150vh] bg-[url(Assets/Images/website-bg.png)] bg-primary`}
+      className={`flex flex-col justify-center items-center bg-cover bg-[position:70%_bottom]
+         md:bg-contain md:bg-[position:center_65%] bg-no-repeat min-h-[165vh] bg-[url(Assets/Images/website-bg.png)] bg-primary`}
     >
       <Header />
       <div className="flex flex-col flex-1 pt-40 md:pt-60 text-center">
         <div>
           <h1 className="text-6xl">John Flynn</h1>
           <div className="pt-3 text-2xl font-thin">
-            <em>WEB DEVELOPER SPECIALIZING IN REACT/REACT NATIVE</em>
+            <em>SOFTWARE DEVELOPER SPECIALIZING IN REACT/REACT NATIVE</em>
           </div>
         </div>
+      </div>
+      <div className="mt-auto max-w-2xl mx-auto pb-10">
+        I’m a React / React Native developer focused on building clean,
+        maintainable mobile experiences. I enjoy collaborating with thoughtful
+        teams and solving real-world problems.
+      </div>
+      <div className="pb-30">
+        <GhostButton text="Read more" />
       </div>
       <Footer />
     </div>
