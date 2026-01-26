d56d708c264b443a916d3e6abb1f699cb2f6ff74
John Flynn <jrflynn3@gmail.com>
2026-01-26 18:42:56 -0500

    add button component

---
+++ src/Components/GhostButton.tsx
@@ -0,0 +1,10 @@
+const GhostButton = ({ text }: { text: string }) => {
+  return (
+    <div className="border border-secondary/35 hover:bg-secondary hover:text-white rounded-md py-1.5 px-3 transition-colors delay-100">
+      {text}
+      <span className="pl-4">→</span>
+    </div>
+  );
+};
+
+export default GhostButton;
