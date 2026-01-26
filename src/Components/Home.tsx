1ac3d49f87bca257c670c6ccbff49d6cb70db97a
John Flynn <jrflynn3@gmail.com>
2026-01-25 21:21:13 -0500

    add backgorund image to home screen

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,12 +3,17 @@ import { Header } from "./Header";
 
 export const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div
+      className={`flex flex-col bg-cover bg-[position:70%_bottom]
+         md:bg-contain md:bg-bottom bg-no-repeat min-h-[150vh] bg-[url(Assets/Images/website-bg.png)] bg-primary`}
+    >
       <Header />
-      <div className="flex flex-col flex-1 justify-center min-h-screen text-2xl text-center bg-primary">
-        <h1>John Flynn</h1>
+      <div className="flex flex-col flex-1 pt-40 md:pt-60 text-center">
         <div>
-          <em>WEB DEVELOPER SPECIALIZING IN REACT/REACT NATIVE</em>
+          <h1 className="text-6xl">John Flynn</h1>
+          <div className="pt-3 text-2xl font-thin">
+            <em>WEB DEVELOPER SPECIALIZING IN REACT/REACT NATIVE</em>
+          </div>
         </div>
       </div>
       <Footer />
