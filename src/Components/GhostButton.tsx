2010f8aaa5ce16df0f8ce697aab53bdc821b9a58
John Flynn <jrflynn3@gmail.com>
2026-01-31 15:25:29 -0500

    adjust button size when changing to mobile

--- src/Components/GhostButton.tsx
+++ src/Components/GhostButton.tsx
@@ -1,6 +1,6 @@
 const GhostButton = ({ text }: { text: string }) => {
   return (
-    <div className="border border-secondary/35 hover:bg-secondary hover:text-white rounded-md py-1.5 px-3 transition-colors delay-100">
+    <div className="border border-secondary/35 hover:bg-secondary hover:text-white rounded-md py-1.5 px-3 transition-colors delay-100 text-sm md:text-base">
       {text}
       <span className="pl-4">→</span>
     </div>
