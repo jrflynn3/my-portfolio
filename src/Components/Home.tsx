fee41e22ba39d9e87dc21f80ced652a24d534f67
John Flynn <jrflynn3@gmail.com>
2026-01-31 15:24:16 -0500

    add different background to home page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -6,11 +6,11 @@ import { Header } from "./Header";
 export const Home = () => {
   return (
     <div
-      className={`flex flex-col justify-center items-center bg-cover bg-[position:70%_bottom]
-         md:bg-contain md:bg-[position:center_65%] bg-no-repeat min-h-[165vh] bg-[url(Assets/Images/website-bg.png)] bg-primary`}
+      className={`flex flex-col items-center bg-cover bg-[position:70%_bottom]
+         md:bg-cover md:bg-top bg-no-repeat min-h-[165vh] bg-[url(Assets/Images/mountains-bg.jpg)] bg-primary`}
     >
       <Header />
-      <div className="flex flex-col flex-1 pt-40 md:pt-60 text-center">
+      <div className="flex flex-col flex-1 pt-40 md:pt-80 text-center">
         <div>
           <h1 className="text-6xl">John Flynn</h1>
           <div className="pt-3 text-2xl font-thin">
@@ -18,16 +18,19 @@ export const Home = () => {
           </div>
         </div>
       </div>
-      <div className="mt-auto max-w-2xl mx-auto pb-10">
-        I’m a React / React Native developer focused on building clean,
-        maintainable mobile experiences. I enjoy collaborating with thoughtful
-        teams and solving real-world problems.
+      <div className="flex flex-col max-w-4xl items-center bg-primary rounded-2xl px-15 py-6">
+        <div className="flex pb-10">
+          I’m a React / React Native developer focused on building clean,
+          maintainable mobile experiences. I enjoy collaborating with thoughtful
+          teams and solving real-world problems.
+        </div>
+        <div className="flex">
+          <Link to="/About">
+            <GhostButton text="Read more" />
+          </Link>
+        </div>
       </div>
-      <div className="pb-30">
-        <Link to="/About">
-          <GhostButton text="Read more" />
-        </Link>
-      </div>
+      <div className="pb-50"></div>
       <Footer />
     </div>
   );
