596e83cbc8c735404f8545cd9bad0f1974ce1faa
John Flynn <jrflynn3@gmail.com>
2026-01-31 16:45:14 -0500

    adjust spacing a bit when in mobile view

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
   return (
     <div
       className={`flex flex-col items-center bg-cover bg-[position:70%_bottom]
-         md:bg-cover md:bg-top bg-no-repeat min-h-[165vh] bg-[url(Assets/Images/mountains-bg.jpg)] bg-primary`}
+         md:bg-cover md:bg-top bg-no-repeat min-h-[200vh] sm:min-h-[165vh] bg-[url(Assets/Images/mountains-bg.jpg)] bg-primary`}
     >
       <Header />
       <div className="flex flex-col flex-1 pt-40 md:pt-80 text-center">
@@ -18,17 +18,15 @@ export const Home = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col max-w-4xl items-center bg-primary rounded-2xl px-15 py-6">
-        <div className="flex pb-10">
+      <div className="flex flex-col max-w-4xl items-center bg-primary rounded-2xl px-7 sm:px-15 py-6">
+        <div className="flex pb-5 max-w-75 sm:max-w-none text-sm sm:text-base">
           I’m a React / React Native developer focused on building clean,
           maintainable mobile experiences. I enjoy collaborating with thoughtful
           teams and solving real-world problems.
         </div>
-        <div className="flex">
-          <Link to="/About">
-            <GhostButton text="Read more" />
-          </Link>
-        </div>
+        <Link to="/About">
+          <GhostButton text="Read more" />
+        </Link>
       </div>
       <div className="pb-50"></div>
       <Footer />
