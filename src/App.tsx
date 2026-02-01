77b0b774862e56aeada9e49820a0726fc04fbecd
John Flynn <jrflynn3@gmail.com>
2026-01-31 21:56:52 -0500

    remove contact page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./Components/Home";
 import { About } from "./Components/About";
 import { Portfolio } from "./Components/Portfolio";
-import { Contact } from "./Components/Contact";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="portfolio" element={<Portfolio />}></Route>
-      <Route path="contact" element={<Contact />}></Route>
       <Route path="about" element={<About />}></Route>
     </Routes>
   );
