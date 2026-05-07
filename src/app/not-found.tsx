b7ebf692dea420c69cedefb010aef5df90518c89
John Flynn <jrflynn3@gmail.com>
2026-05-06 21:54:04 -0400

    replace old metadata

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import { GhostButton } from "@/Components/common";
-import { PageHead } from "@/Components/common/PageHead";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404 Not Found | John Flynn",
+  description: "Page not found",
+};
 
 export const NotFound = () => {
   return (
     <>
-      <PageHead
-        title="404 Not Found | John Flynn"
-        description="Page Not Found"
-      />
       <div className="flex flex-col items-center justify-center min-h-screen bg-primary text-center px-4">
         <h1 className="text-6xl font-bold pb-4">404</h1>
         <p className="text-xl font-thin pb-8">Page not found</p>
