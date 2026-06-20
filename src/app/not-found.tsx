1aabb0ff56113557e65d848faf50cc8e2685249d
John Flynn <jrflynn3@gmail.com>
2026-06-20 18:58:05 -0400

    adjust 404 spacing

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,14 +9,12 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-primary text-center px-4">
-        <h1 className="text-6xl font-bold pb-4">404</h1>
-        <p className="text-xl font-thin pb-8">Page not found</p>
-        <Link href="/">
-          <GhostButton text="Go home" />
-        </Link>
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-primary text-center px-4 pb-header">
+      <h1 className="text-6xl font-bold pb-4">404</h1>
+      <p className="text-xl font-thin pb-8">Page not found</p>
+      <Link href="/">
+        <GhostButton text="Go home" />
+      </Link>
+    </div>
   );
 }
