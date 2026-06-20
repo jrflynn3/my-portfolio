8badd11bbb6aede532d89b83290f1d8774e6b2fe
John Flynn <jrflynn3@gmail.com>
2026-06-20 18:37:37 -0400

    remove commented code

--- src/env.ts
+++ src/env.ts
@@ -16,21 +16,3 @@ export const env = createEnv({
     RESEND_EMAIL: process.env.RESEND_EMAIL,
   },
 });
-
-// const envSchema = z.object({
-//   RESEND_API_KEY: z.string().min(1, "RESEND_API_KEY is required"),
-//   RESEND_CONTACT_EMAIL: z.email("RESEND_CONTACT_EMAIL must be a valid email."),
-//   RESEND_EMAIL: z.email("RESEND_EMAIL must be a valid email."),
-// });
-
-// const parsed = envSchema.safeParse(process.env);
-
-// if (!parsed.success) {
-//   console.error(
-//     "Invalid environment variables:\n",
-//     z.prettifyError(parsed.error),
-//   );
-//   throw new Error("Invalid environment variables");
-// }
-
-// export const env = parsed.data;
